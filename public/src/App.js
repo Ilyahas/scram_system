0709e7f2c85d902f41a6dddd644d6af08ea1c7d2
User <user@Mac-mini-alex.local>
2018-03-25 04:29:26 +0300

    fix

--- public/src/App.js
+++ public/src/App.js
@@ -1,7 +1,7 @@
-import  "./App.scss";
+import "./App.scss";
 import React from 'react'
 import PropTypes from 'prop-types'
-import {Route, Switch } from 'react-router-dom'
+import { Route, Switch } from 'react-router-dom'
 import WelcomePage from "./containers/pages/WelcomePage";
 import SignupPage from "./containers/pages/SignupPage";
 import LoginPage from "./containers/pages/LoginPage";
@@ -11,20 +11,22 @@ import EmailConfirm from './containers/pages/EmailConfirm'
 import GuestRoute from "../src/containers/routes/GuestRoute";
 import UserRoute from "../src/containers/routes/UserRoute";
 //TODO: props type check
-const App = ({location}) => {
+const App = ({ location }) => {
   return (
     <Switch>
-        <GuestRoute location={location} path="/" exact component={WelcomePage}></GuestRoute>
-        <GuestRoute location={location} path="/auth/signup" exact component={SignupPage}></GuestRoute>
-        <GuestRoute location={location} path="/auth/login" exact component={LoginPage}></GuestRoute>
-        <Route localtion={location} path="/auth/confirmation/:token" exact component={EmailConfirm}> </Route>
-        <UserRoute location={location} path="/home" exact component={HomePage}></UserRoute> 
+      <GuestRoute location={location} path="/" exact component={WelcomePage}></GuestRoute>
+      <GuestRoute location={location} path="/auth/signup" exact component={SignupPage}></GuestRoute>
+      <GuestRoute location={location} path="/auth/login" exact component={LoginPage}></GuestRoute>
+      <Route localtion={location} path="/auth/confirmation/:token" exact component={EmailConfirm}> </Route>
+      <UserRoute location={location} path="/home" exact component={HomePage}></UserRoute>
+      {/* <Route path='/404' component={My404Component} />
+<Redirect from='*' to='/404' /> */}
     </Switch>
   )
 }
 
-App.propTypes={
-  location:PropTypes.shape({
+App.propTypes = {
+  location: PropTypes.shape({
     pathname: PropTypes.string.isRequired
   }).isRequired
 }
