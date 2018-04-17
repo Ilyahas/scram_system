fd7c314f5cee925ebd8521854424fdd40bfcd2b8
User <user@Mac-mini-alex.local>
2018-04-17 19:04:58 +0300

    new build

--- public/src/containers/pages/HomePage/index.js
+++ public/src/containers/pages/HomePage/index.js
@@ -2,7 +2,11 @@ import React, { Component } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Home from '../../../components/Home'
+import {token} from '../../../actions/auth'
 export class HomePage extends Component {
+  componentDidMount(){
+    this.props.token(localStorage.JWT)
+  }
   render() {
     return (
      <Home/>
@@ -15,7 +19,7 @@ const mapStateToProps = (state) => ({
 })
 
 const mapDispatchToProps = {
-  
+  token
 }
 
 export default connect(mapStateToProps, mapDispatchToProps)(HomePage)
